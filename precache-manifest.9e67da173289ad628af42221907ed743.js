b398743fe6e166f2ce437b328de280c4d80b3d3a
Acarvajal905 <acarvajal905@gmail.com>
2020-10-14 17:10:25 -0300

    Updates

---
+++ precache-manifest.9e67da173289ad628af42221907ed743.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e479b4a3a7445bf4320edbdafb444b0b",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "05255e29d26f0cd3e76d",
+    "url": "/react-tic-tac-toe/static/css/main.af9650da.chunk.css"
+  },
+  {
+    "revision": "7de1f036a2157dac2058",
+    "url": "/react-tic-tac-toe/static/js/2.57343eaf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tic-tac-toe/static/js/2.57343eaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05255e29d26f0cd3e76d",
+    "url": "/react-tic-tac-toe/static/js/main.82b484f5.chunk.js"
+  },
+  {
+    "revision": "929d476f7b1ee9149c3a",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.db7cfeb9.js"
+  }
+]);
